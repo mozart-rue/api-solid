f2d98088029f23898e1a6a9e2823d7b329128ec0
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2023-04-11 22:07:21 -0300

    feat: configured environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import {app} from './app';
+import { env } from './env';
 
 
 app.listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
 }).then(() => {console.info("HTTP Server Running!")})
