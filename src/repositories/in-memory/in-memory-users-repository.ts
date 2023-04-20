42f24e540ea29c678742d111a09acb31556b8f53
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2023-04-19 21:49:11 -0300

    feat: create method findById

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -4,6 +4,16 @@ import { UserRepository } from '../users-repository';
 export class InMemoryUsersRepository implements UserRepository {
   public items: User[] = [];
 
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+
   async findByEmail(email: string) {
     const user = this.items.find((item) => item.email === email);
 
