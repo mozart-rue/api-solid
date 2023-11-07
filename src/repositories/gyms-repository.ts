6dbdfcc470d78c107e25c0805f26539d3e9ff52a
Mozart Rueckert <mozartrue@outlook.com>
2023-11-06 23:19:37 -0300

    fix: changed parameter of create method

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,6 +1,6 @@
-import { Gym } from '@prisma/client';
+import { Gym, Prisma } from '@prisma/client';
 
 export interface GymRepository {
   findById(id: string): Promise<Gym | null>;
-  create(gym: Gym): Promise<Gym | null>;
+  create(data: Prisma.GymCreateInput): Promise<Gym>;
 }
