b074ac7f79ac8e3543d312cc8c9a96e4ed7bde9d
Mozart Rueckert <mozartrue@outlook.com>
2023-11-06 23:20:49 -0300

    fix: create object gym when create a new one

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,12 +1,21 @@
-import { Gym } from '@prisma/client';
+import { Gym, Prisma } from '@prisma/client';
 import { GymRepository } from '../gyms-repository';
+import { randomUUID } from 'node:crypto';
 
 export class InMemoryGymsRepository implements GymRepository {
   public items: Gym[] = [];
 
-  async create(gym: Gym) {
+  async create(data: Prisma.GymCreateInput) {
+    const gym: Gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+    }
+
     this.items.push(gym);
-    
     return gym;
   }
 
