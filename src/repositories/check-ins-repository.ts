507698c76323d3762572685324c97e3bf2a0fc40
Mozart Rueckert <mozartrue@outlook.com>
2023-11-07 19:19:13 -0300

    feat: set method findManyById

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,6 +1,7 @@
-import { CheckIn, Prisma } from '@prisma/client';
+import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInsRepository {
-  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
-  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
 }
