427ffb519e4abd77fe8224a938966368d724965d
Mozart Rueckert <mozartrue@outlook.com>
2023-11-06 23:22:13 -0300

    feat: create use case to create a gym

---
+++ src/use-cases/create-gym.ts
@@ -0,0 +1,37 @@
+import { Gym, User } from '@prisma/client';
+import { GymRepository } from '@/repositories/gyms-repository';
+
+interface ICreateGymUseCaseRequest {
+  title: string;
+  description: string | null;
+  phone: string | null;
+  longitude: number;
+  latitude: number;
+}
+
+interface ICreateGymUseCaseResponse {
+  gym: Gym;
+}
+
+export class CreateGymUseCase {
+  constructor(private gymRepostory: GymRepository) {}
+
+  async execute({
+    title,
+    description,
+    phone,
+    longitude,
+    latitude
+  }: ICreateGymUseCaseRequest): Promise<ICreateGymUseCaseResponse> {
+
+    const gym = await this.gymRepostory.create({
+      title,
+      description,
+      phone,
+      longitude,
+      latitude
+    });
+
+    return { gym };
+  }
+}
