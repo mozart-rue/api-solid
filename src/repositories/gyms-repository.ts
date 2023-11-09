70115c124eee36992cbe9008dbc62dae4c1e2cdc
Mozart Rueckert <mozartrue@outlook.com>
2023-11-08 21:32:16 -0300

    feat: set method searchMany

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,6 +1,7 @@
-import { Gym, Prisma } from '@prisma/client';
+import { Gym, Prisma } from '@prisma/client'
 
 export interface GymRepository {
-  findById(id: string): Promise<Gym | null>;
-  create(data: Prisma.GymCreateInput): Promise<Gym>;
+  findById(id: string): Promise<Gym | null>
+  searchMany(query: string, page: number): Promise<Gym[]>
+  create(data: Prisma.GymCreateInput): Promise<Gym>
 }
