d4f39af93f5f95aa8ac02ffef4157aaa82c0c8e0
Mozart Rueckert <mozartrue@outlook.com>
2023-11-08 22:23:11 -0300

    feat: set method save

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -6,4 +6,5 @@ export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   countByUserId(userId: string): Promise<number>
+  save(checkIn: CheckIn): Promise<CheckIn>
 }
