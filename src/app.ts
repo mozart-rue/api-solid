faaec73216f786c10b847cc3ac6c7680a043c062
Mozart Rueckert <mozartrue@outlook.com>
2023-11-09 22:06:19 -0300

    feat: register jwt secret to app routes

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,29 @@
-import fastify from 'fastify';
-import { appRoutes } from './http/routes';
-import { ZodError } from 'zod';
-import { env } from './env';
+import fastify from 'fastify'
+import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
+import fastifyJwt from '@fastify/jwt'
 
-export const app = fastify();
+export const app = fastify()
 
-app.register(appRoutes);
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(appRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
       .status(400)
-      .send({ message: 'Validation Error', issues: error.format() });
+      .send({ message: 'Validation Error', issues: error.format() })
   }
 
   if (env.NODE_ENV !== 'production') {
-    console.error(error);
+    console.error(error)
   } else {
     // TODO Here we should log to and external tool like DataDog/NewRelic/Sentry
   }
 
-  return reply.status(500).send({ message: 'Internal server error' });
-});
+  return reply.status(500).send({ message: 'Internal server error' })
+})
