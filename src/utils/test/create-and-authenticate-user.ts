5e286bc6c9bde4fc1d735daeb0f48f810c691c44
Mozart Rueckert <mozartrue@outlook.com>
2023-11-12 14:48:21 -0300

    feat: create a user with variable role

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,16 +1,24 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    password: 'minhasenha123',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('senha123', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
     email: 'johndoe@example.com',
-    password: 'minhasenha123',
+    password: 'senha123',
   })
 
   const { token } = authResponse.body
