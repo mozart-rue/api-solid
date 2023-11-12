ff153faaf438e6d3cd4d80ab1f343cde60d32012
Mozart Rueckert <mozartrue@outlook.com>
2023-11-12 14:42:50 -0300

    feat: add role as user field

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -3,6 +3,7 @@ import '@fastify/jwt'
 declare module '@fastify/jwt' {
   interface FastifyJWT {
     user: {
+      role: 'ADMIN' | 'MEMBER'
       sub: string
     } // user type is return type of `request.user` object
   }
