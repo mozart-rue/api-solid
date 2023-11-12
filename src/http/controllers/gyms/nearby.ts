a24b4c14caa5df0a073d2524d3c904a94523ee32
Mozart Rueckert <mozartrue@outlook.com>
2023-11-12 11:32:15 -0300

    fix: converted received param from string to number

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -4,15 +4,15 @@ import { z } from 'zod'
 
 export async function nearby(request: FastifyRequest, reply: FastifyReply) {
   const nearbyGymsQuerySchema = z.object({
-    latitude: z.number().refine((value) => {
+    latitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 90
     }),
-    longitude: z.number().refine((value) => {
+    longitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 180
     }),
   })
 
-  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.query)
 
   const nearbyGymsUseCase = makeFetchNearbyGymsUseCase()
 
